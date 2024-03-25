90c42294dfc7d8a840fd0a1d6ead411d2656ef73
Sayan Deb <keepimaginingsayan@gmail.com>
2024-03-26 02:19:52 +0530

    Initial Commit

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceImage1: UIImageView!
+    @IBOutlet weak var diceImage2: UIImageView!
+    
+
+    @IBAction func ActionButton(_ sender: UIButton) {
+        
+        //This line below denotes that we have an array called the diceArray and it contains all the possible ice images
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        //This line bwloe calls the array and .randomElement() chooses an random element from the array
+        diceImage1.image = diceArray.randomElement()
+        diceImage2.image = diceArray.randomElement()
+        
+        //Similarly we can also use
+        //diceImage2.image = diceArray[Int.random(in: 0...5)]
+        //This above line uses the 
+        
     }
-
-
 }
 
